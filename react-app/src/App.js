c9b882f3a53583b19443699bfa379bbcbbe7bf52
mairaGalvao <mairagalvao01@gmail.com>
2021-11-23 17:25:03 -0300

    maps created, switched on and off deafult color, pins with coordinates linked

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,89 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import { Icon } from 'leaflet'
-import covidData from './data.json'
-import icon1 from './images/covid19.svg'
-// import { LatLngExpression } from 'leaflet';
+import { divIcon } from 'leaflet';
+import './App.css';
 
 
 function App() {
+  const [ activeInfo, setActiveInfo ] = useState( null );
   const [position, setPosition] = useState([0,0])
-  //saving the information to be able to render
-  const [serverResponseZero, setServerResponseZero] = useState()
-  // const [serverResponseOne, setServerResponseOne] = useState()
+  const [featuresArray, setFeaturesArray] = useState()
+  const [defaultColor, setDefaultColor] = useState(true)
 
-
-  const testCoordinates = [[ 80.593683, 78.962883 ], [ 200.593683, 400.962883 ], [ 500.593683, 800.962883 ]]
-
-// serverResponse = value
-//setServerResponse is a function with the value as parameter
   async function getData() {
-    console.log("fetching data")
     const response = await fetch('http://localhost:5000/mapview');
-    console.log("fetching data in json")
     const jsonResponse = await response.json();
-    // setServerResponse(jsonResponse)
-    // console.log("fetching the json FEATURES")
-    // var jsonFeatures = jsonResponse.features
-    var jsonFeaturesZero = jsonResponse.features[0].geometry.coordinates
-    console.log(jsonFeaturesZero, '<<<< ZERO ')
-    // var jsonFeaturesOne = jsonResponse.features[0].geometry.coordinates[0]
-    // console.log(jsonFeaturesOne, '<<<< ONE ')
-    setServerResponseZero(jsonFeaturesZero)
-    // setServerResponseOne(jsonFeaturesOne)
+    const teste = jsonResponse.features
+    var featuresArray = jsonResponse.features.slice(1,7)    // todo remove slice!
+    setFeaturesArray(featuresArray)
   }
 
   useEffect(() => {
-    console.log("in use effect!")
     getData();
   }, []);
-  // const [ activeCovid, setActiveCovid ] = useState( null );
 
 
-  return (<>
+  const markerHtmlStyles = (color) => {
+    if (defaultColor == false) {
+      var iconColor = color
+    }
+    else{
+      iconColor = 'grey'
+    }
+    return `background-color: ${iconColor};
+    width: 1rem;
+    height: 1rem;
+    display: block;
+    left: -1.5rem;
+    top: -1.5rem;
+    position: relative;
+    border-radius: 3rem 3rem 0;
+    transform: rotate(45deg);
+    border: 1px solid #FFFFFF`
+  } 
+
+  const icon = (color) => {
+    const obejctStyle = markerHtmlStyles(color)
+    const htmlString = `<span style="${obejctStyle}" />`
+  return  divIcon({
+    className: "my-custom-pin",
+    iconAnchor: [0, 24],
+    labelAnchor: [-6, 0],
+    popupAnchor: [0, -36],
+    html: htmlString
+  })}
+
+  const switchColors = () => {
+    setDefaultColor(!defaultColor)
+  }
 
 
+return (<>
+
+    <button onClick={switchColors}> Switch </button>
  <MapContainer 
-          position = { [ 80.593683, 78.962883 ] }
+          center = {[8.382046, 11.6146463 ]}
           center={position} 
-          // position = 'topRight'
-    
-          zoom = { 5 }
+            zoom = { 1 }
           scrollWheelZoom = { true } 
-
       >
       <TileLayer 
         attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         url = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
       />
-
-
        <TileLayer 
           attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' 
           url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
         />
-  
-    
-  {serverResponseZero && serverResponseZero.map((anObjectMapped, index) => {
-    console.log(anObjectMapped)     // [x, y]
-    console.log(index)
-    return (
-      
-        <Marker key={index} 
-        position={[anObjectMapped[0], anObjectMapped[1]]}
-          >
+   {featuresArray && featuresArray.map((featureObject, idx1) => { 
+     return featureObject.geometry.coordinates.map((singleCoordinate, idx2) => {
+      const myid = idx1 + '_' + idx2 
+       return (<Marker       key={myid}  position={singleCoordinate}
+        icon= {icon(featureObject.properties.color)}
+         > 
             
-        </Marker>
-        
-    );
-})}
-
-  
-  
+       </Marker>)
+      
+     }
+     )})} 
 
   </MapContainer> 
+
 
   </>
   );
